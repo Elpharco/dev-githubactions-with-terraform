01a45e871caa35f5cb3bd8d0f5c78f86071757b2
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-02 00:50:01 +0100

    modified backend.tf to providers info

--- backend.tf
+++ backend.tf
@@ -4,4 +4,11 @@ terraform {
     key    = "devtest-github-actions.tfstate"
     region = "us-east-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 0.14"
+    }
+  }
 }
