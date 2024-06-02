fe853f098493e9576be268a89263c20399101cc9
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-02 01:28:44 +0100

    updated terraform version to match worklow

--- backend.tf
+++ backend.tf
@@ -5,10 +5,10 @@ terraform {
     region = "us-east-1"
   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 0.14"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "~> 0.14"
+  #   }
+  # }
 }
