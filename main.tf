28274c31ff932fd203ad03670c2b8b07ff829584
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-11 01:14:18 +0100

    modified main.tf file02

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "git_ec2_dev-test" {
+resource "aws_instance" "ec2_dev" {
 
   ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "git_ec2_dev-test"
+    Name = "my_ec2_dev"
 
   }
 
