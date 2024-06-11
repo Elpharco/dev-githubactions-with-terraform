b9b67d6b0ad6d87dc9218bef6ebdb95a180696e4
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-11 01:22:17 +0100

    modified main.tf file04

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
-
-resource "aws_instance" "ec2_dev-test" {
-
-  ami           = "ami-02d7fd1c2af6eead0"
-  instance_type = "t3.small"
-  
-  
+resource "aws_vpc" "VPC-test-pipeline" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "git_ec2_dev-test"
-
+    Name = "VPC_pipeline"
   }
-
 }
+
+
+# resource "aws_instance" "ec2_dev-test" {
+
+#   ami           = "ami-02d7fd1c2af6eead0"
+#   instance_type = "t2.micro"
+  
+  
+
+#   tags = {
+#     Name = "git_ec2_dev-test"
+
+#   }
+
+# }
