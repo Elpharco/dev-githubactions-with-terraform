640a41abac5f48dc122efb4d0763f357e6ab7216
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-11 14:16:30 +0100

    added variables file, updated main.tf and provider.tf files to that effect

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_dev" {
 
-  ami           = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  ami           = var.os-name
+  instance_type = var.instance-type
 
   tags = {
     Name = "git_ec2_dev"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "os-name" {
+  default = "ami-02d7fd1c2af6eead0"
+}
+
+variable "instance-type" {
+  default = "t2.micro"
+}
