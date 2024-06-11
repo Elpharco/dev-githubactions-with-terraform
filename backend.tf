6fffad0eb5b737f4de3d766f8cb85e19f764d9b7
stanley <stanleyc.nwachukwu@gmail.com>
2024-06-11 01:09:30 +0100

    updated main.yml-03

--- backend.tf
+++ backend.tf
@@ -12,3 +12,18 @@ terraform {
   #   }
   # }
 }
+
+
+
+
+
+#   terraform {
+#   backend "s3" {
+#     encrypt = true    bucket = "hella-buckets"
+#     dynamodb_table = "terraform-state-lock-dynamo"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
+  
